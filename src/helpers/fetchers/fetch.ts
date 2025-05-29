92b4c6e78cbc81197388a01eada957c55fd85c25
Velenir <velenir@gmail.com>
2025-05-29 19:06:39 +0200

    cleanup

--- src/helpers/fetchers/fetch.ts
+++ src/helpers/fetchers/fetch.ts
@@ -34,7 +34,6 @@ export const constructFetcher =
             }
           : undefined;
 
-      // console.error('🚀 ~ url:', url);
       const response = await fetch(url, {
         method,
         body,
