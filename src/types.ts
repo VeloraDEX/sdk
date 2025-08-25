f99b732055b0550659199413534318838069e0d7
Velenir <velenir@gmail.com>
2025-08-25 16:33:50 +0200

    cleanup

--- src/types.ts
+++ src/types.ts
@@ -56,14 +56,9 @@ export interface FetcherPostInput<URL extends string = string>
   method: 'POST';
   data: Record<string, any>;
 }
-export interface FetcherDeleteInput<URL extends string = string>
-  extends FetcherInputBase<URL> {
-  method: 'DELETE';
-  data: Record<string, any>;
-}
 
 export type FetcherFunction = <T, URL extends string = string>(
-  params: FetcherGetInput<URL> | FetcherPostInput<URL> | FetcherDeleteInput<URL>
+  params: FetcherGetInput<URL> | FetcherPostInput<URL>
 ) => Promise<T>;
 
 // authentication or some other params required in `fetcher`
