cc119aae485affd7ba38fdf17098f83ddf61f2da
Velenir <velenir@gmail.com>
2025-11-14 10:21:39 +0100

    OrdersFilter.status/update internal statuses

--- src/methods/delta/getDeltaOrders.ts
+++ src/methods/delta/getDeltaOrders.ts
@@ -41,6 +41,7 @@ type OrdersFilter = {
    * Filter by any known DeltaAuctionStatus and some custom statuses:
    * - **INSUFFICIENT_BALANCE** —  returned as SUSPENDED from API
    * - **INSUFFICIENT_ALLOWANCE** —  returned as SUSPENDED from API
+   * - **AWAITING_PRE_SIGNATURE** —  returned as SUSPENDED from API
    * - **INVALIDATED** —  returned as FAILED from API
    * - **ACTIVE** —  All orders with NOT_STARTED, RUNNING, EXECUTING or SUSPENDED statuses.
    * - **INACTIVE** —  All orders with EXECUTED, FAILED, EXPIRED, CANCELLED or INVALIDATED statuses.
